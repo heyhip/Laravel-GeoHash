c385187e165b2497e8ae03365e80979cdaf5ca24
YouthAge <myouthage@gmail.com>
2018-06-27 12:00:26 +0800

    延迟加载

--- src/GeoHashProvider.php
+++ src/GeoHashProvider.php
@@ -7,6 +7,13 @@ use Illuminate\Support\ServiceProvider;
 class GeoHashProvider extends ServiceProvider
 {
     /**
+     * 服务提供者加是否延迟加载
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
      * Bootstrap services.
      *
      * @return void
@@ -28,4 +35,16 @@ class GeoHashProvider extends ServiceProvider
             return new GeoHash();
         });
     }
+
+    /**
+     * 获取由提供者提供的服务
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['geohash'];
+    }
+
+
 }
